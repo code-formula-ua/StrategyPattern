93db4bb16adaa730445da45b8c184a0d16fd9916
code-formula-ua <sliusarenko.channels@gmail.com>
2026-08-18 13:45:20 +0300

    Add project files.

---
+++ StrategyPattern/DeliveryService.cs
@@ -0,0 +1,19 @@
+﻿public class DeliveryService
+{
+    public void Deliver(string type)
+    {
+        if (type == "standard")
+        {
+            Console.WriteLine("Standard delivery");
+        }
+        else if (type == "express")
+        {
+            Console.WriteLine("Express delivery");
+        }
+        else if (type == "same-day")
+        {
+            Console.WriteLine("Same-day delivery");
+        }
+        //if else???
+    }
+}
---
+++ StrategyPattern/Program.cs
@@ -0,0 +1,80 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var service = new DeliveryService();
+        service.Deliver("express");
+
+
+        IDeliveryStrategy deliveryStrategy;
+
+        bool isExpress = false;
+
+        if (isExpress)
+        {
+            deliveryStrategy = new ExpressDelivery();
+        }
+        else
+        {
+            deliveryStrategy = new DroneDelivery();
+        }
+
+
+        var strategyService = new DeliveryStrategyService(deliveryStrategy);
+
+        strategyService.Deliver();
+
+    }
+}
+
+public interface IDeliveryStrategy
+{
+    void Deliver();
+}
+
+public class DroneDelivery : IDeliveryStrategy
+{
+    public void Deliver()
+    {
+        Console.WriteLine("Drone strategy delivery");
+    }
+}
+public class StandartDelivery : IDeliveryStrategy
+{
+    public void Deliver()
+    {
+        Console.WriteLine("Standart strategy delivery");
+    }
+}
+
+public class ExpressDelivery : IDeliveryStrategy
+{
+    public void Deliver()
+    {
+        Console.WriteLine("Express strategy delivery");
+    }
+}
+
+public class SameDayDelivery : IDeliveryStrategy
+{
+    public void Deliver()
+    {
+        Console.WriteLine("Sameday strategy delivery");
+    }
+}
+
+public class DeliveryStrategyService
+{
+    private readonly IDeliveryStrategy _strategy;
+
+
+    public DeliveryStrategyService(IDeliveryStrategy strategy)
+    {
+        _strategy = strategy;
+    }
+
+    public void Deliver()
+    {
+        _strategy.Deliver();
+    }
+}
